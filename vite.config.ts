61d9db5f89049a130483b6b3ef3c813e4b021d80
IvanMcNish <89428777+IvanMcNish@users.noreply.github.com>
2025-12-10 19:34:49 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/PortalFacturas/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    // 👇 MUY IMPORTANTE para GitHub Pages
+    base: '/PortalFacturas/',
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+  };
 });
